990a7f95b7bb0d7825e54b897a2007a546d308da
berger <robertberger.dev@gmail.com>
2023-02-13 17:55:41 -0500

    finished Assignment

--- src/Experience.js
+++ src/Experience.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './Experience.module.css'
 
 class Experience extends React.Component {
     render () {
         let { data } = this.props;
         return (
-            <div>
+            <div class={styles.experience}>
                 <h1>{data.companyName}, {data.jobTitle}</h1>
                 <p>{data.description}</p>
             </div>
