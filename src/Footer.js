c57d70eba6e9d73ef12f63800a393b4996aed0b5
berger <robertberger.dev@gmail.com>
2023-02-13 16:22:15 -0500

    added Footer function and got it to render on the webpage

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Footer.module.css'
 
 function Footer() {
-    return (<a href='https://github.com/robberger'></a>)
+    return (<a href='https://github.com/robberger'>My Github Page</a>)
 }
 
 export default Footer
